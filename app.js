d1e1b867d8d375342fef4b1675f703b04d531bd6
kolaveridi <coderboysatya@gmail.com>
2018-04-07 23:09:47 +0530

    google auth

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const app=express();
+const passport=require('passport');
+//passport config
+require('./config/passport')(passport);
+//port setup
 const port=process.env.PORT || 3000;
+//load routes
+const auth=require('./routes/auth');
+
+
 app.get('/',(req,res)=>{
   res.send('It works');
 });
+//use routes
+app.use('/auth',auth);//anything with auth will go to auth.js
+
 app.listen(port,()=>{
   console.log(`Server started on ${port}`);
 });
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+  momgoURI:'mongodb://chipmunksatyajeet:$satyajeet$12@ds237379.mlab.com:37379/storybooks-dev',
+  googleClientID:'109640107073-uq6t7rdt64eoh9ubte7obnfu2pm3vpe1.apps.googleusercontent.com',
+  googleClientSecret:'yJoG-Y-GdqTpprI9s72oYD3Q'
+}
